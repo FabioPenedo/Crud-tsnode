b7672b3b0388aee4937cac0160d44cfff0d8b172
Fabio Penedo <fabiopenedo21@gmail.com>
2022-01-20 15:34:12 -0300

    base criada

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import * as HomeController from '../controllers/HomeController'
+
+const router = Router()
+
+router.get('/', HomeController.home)
+
+export default router
